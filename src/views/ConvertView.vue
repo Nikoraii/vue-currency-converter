9e107269c97f6036ffc7612a3fbdd4422416dabc
Nikoraii <kolja.st@gmail.com>
2023-09-13 02:42:39 +0200

    Add ConvertView using ConvertComponent

--- src/views/ConvertView.vue
+++ src/views/ConvertView.vue
@@ -1,5 +1,24 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="p-2 mb-6 border-b border-emerald-50">
+    <h1 class="text-center text-6xl">CONVERTER</h1>
+    <p class="text-center">Check out the full list of available currencies 
+      <a href="https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.json" 
+      target="_blank"
+      class="text-emerald-600 font-semibold"
+      >here</a>.
+    </p>
   </div>
+    <ConvertComponent />
 </template>
+
+<script>
+import ConvertComponent from '@/components/ConvertComponent.vue'
+
+export default {
+  name: 'ConvertView',
+  components: {
+      ConvertComponent
+  }
+};
+
+</script>
