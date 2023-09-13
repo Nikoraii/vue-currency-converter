8ea70ae1494fe140f22b01ef188fd0f6de1faae3
Nikoraii <kolja.st@gmail.com>
2023-09-13 03:08:45 +0200

    Update GitHub link

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,7 @@
         </main>
 
         <footer class="w-full bg-slate-100 py-1 border-t border-gray-200">
-            <div class="w-full px-8 text-center text-gray-500">&#169;{{ new Date().getFullYear() }} Made by <a class="text-sky-700" href="https://github.com/Nikoraii" target="_blank">Nikoraii</a></div>
+            <div class="w-full px-8 text-center text-gray-500">&#169;{{ new Date().getFullYear() }} Made by <a class="text-sky-700" href="https://github.com/Nikoraii/vue-currency-converter" target="_blank">Nikoraii</a></div>
         </footer>
     </div>
 </template>
