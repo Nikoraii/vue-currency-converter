63639c686e587a69f326f6afe8554b0538ff98ff
Nikoraii <kolja.st@gmail.com>
2023-09-13 02:48:46 +0200

    Remove unused routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
-import AboutView from '@/views/AboutView.vue'
 import ConvertView from '@/views/ConvertView.vue'
 
 const routes = [
@@ -13,15 +12,6 @@ const routes = [
     path: '/convert',
     name: 'convert',
     component: ConvertView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    // component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-    component: AboutView
   }
 ]
 
