616f4e0a6d396160b918990103aac13a5ebc9cb2
Nikoraii <kolja.st@gmail.com>
2023-09-13 02:37:00 +0200

    Add icons and HomeView template

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,13 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="text-center  rounded-lg  mt-5 px-4 py-2">
+    <h1 class="text-6xl font-semibold text-emerald-600">Currency Converter</h1>
+    <p class="mt-7 ">Convert over 150 currencies includins some cryptocurrencies as well. Built using <a href="https://github.com/fawazahmed0/currency-api#readme"
+      class="underline decoration-rose-600 decoration-2 underline-offset-4 text-emerald-600 font-semibold" target="_blank">Currency-API</a></p>
+    <div class="text-center mt-4">
+      <p>Technologies used:</p>
+      <div class="mt-5">
+        <router-link class="text-2xl font-semibold rounded-lg bg-emerald-500 text-white py-2 px-4" to="/convert">Convert</router-link>
+      </div>
+    </div>
   </div>
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
-</script>
