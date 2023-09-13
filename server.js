7496e9fa12531bc79c2d28d0bb2e403c5940e6dd
Nikoraii <kolja.st@gmail.com>
2023-09-13 02:27:16 +0200

    Add route to convert money

--- server.js
+++ server.js
@@ -21,6 +21,18 @@ app.get('/api/convert', (req, res) => {
   });
 });
 
+app.post('/api/convert', (req, res) => {
+  const convertUrl = 'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies';
+  const { fromCurrency, toCurrency, convertMoney } = req.body;
+  axios.get(`${convertUrl}/${fromCurrency}.json`)
+  .then(response => {
+    const data = response.data;
+    const currencyValues = data[fromCurrency];
+    const convertedRoundedMoney = (currencyValues[toCurrency] * convertMoney).toFixed(2);
+    res.json(convertedRoundedMoney);
+  });
+});
+
 app.listen(3000, () => {
   console.log('Server started on port 3000');
 });
