68c6eac17369fc31ae8e6bcfa0a445f6d5fd9ec4
Nikoraii <kolja.st@gmail.com>
2023-09-13 02:37:35 +0200

    Add icons to HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,6 +5,12 @@
       class="underline decoration-rose-600 decoration-2 underline-offset-4 text-emerald-600 font-semibold" target="_blank">Currency-API</a></p>
     <div class="text-center mt-4">
       <p>Technologies used:</p>
+      <div>
+          <img class="inline" src="@/assets/icons8-vue-js-48.png">
+          <img class="inline pl-4 pr-2" src="@/assets/icons8-nodejs-48.png">
+          <img class="inline pr-4 pl-2" src="@/assets/icons8-express-js-50.png">
+          <img class="inline" src="@/assets/icons8-tailwindcss-48.png">
+      </div>
       <div class="mt-5">
         <router-link class="text-2xl font-semibold rounded-lg bg-emerald-500 text-white py-2 px-4" to="/convert">Convert</router-link>
       </div>
