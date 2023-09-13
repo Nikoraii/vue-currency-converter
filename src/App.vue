f2f3bd120b6ba3b467b398463656cea391901b2e
Nikoraii <kolja.st@gmail.com>
2023-09-13 02:33:29 +0200

    Main App setup

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,24 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="flex flex-col min-h-screen">
+        <header class="border-b border-gray-200 bg-white  w-full">
+            <div class="w-full">
+                <nav class="p-4 flex">
+                    <div class="text-lg font-medium mx-2">
+                        <router-link to="/">Home</router-link>
+                    </div>
+                    <div class="text-lg font-medium mx-2">
+                        <router-link to="/convert">Convert</router-link>
+                    </div>
+                </nav>
+            </div>
+        </header>
+
+        <main class="container mx-auto p-4 mb-auto w-full">
+            <router-view />
+        </main>
+
+        <footer class="w-full bg-slate-100 py-1 border-t border-gray-200">
+            <div class="w-full px-8 text-center text-gray-500">&#169;{{ new Date().getFullYear() }} Made by <a class="text-sky-700" href="https://github.com/Nikoraii" target="_blank">Nikoraii</a></div>
+        </footer>
+    </div>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
